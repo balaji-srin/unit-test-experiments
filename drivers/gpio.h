8d1067daa9c1d25a16599c63e287d0e1a7eae13f
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-09-05 22:17:00 +0200

    Add fff based test (#1)
    * feat: First unit test created with unity and fff
    
    Unity and fff are now git submodules.
    
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

---
+++ drivers/gpio.h
@@ -0,0 +1,10 @@
+#ifndef __GPIO_H
+
+/** API to initialize GPIO.
+ *
+ * @retval 0 on succcess
+ * @retval -1 on failure
+ */
+int gpio_init(void);
+
+#endif
---
+++ examples/led_driver_test/led.c
@@ -0,0 +1,8 @@
+#include "gpio.h"
+
+int led_init(void)
+{
+	gpio_init();
+
+	return 0;
+}
