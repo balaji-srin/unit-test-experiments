e94fc485e6a08e828619ad1654a18bc6e5caf8f7
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-09-07 10:39:40 +0200

    feat: Make led_init return fail on gpio_init fail
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

--- examples/led_driver_test/led.c
+++ examples/led_driver_test/led.c
@@ -2,7 +2,5 @@
 
 int led_init(void)
 {
-	gpio_init();
-
-	return 0;
+	return gpio_init();
 }
--- examples/led_driver_test/led_test.c
+++ examples/led_driver_test/led_test.c
@@ -20,11 +20,18 @@ void tearDown(void)
 
 void test_when_led_init_is_called_it_calls_gpio_init_and_returns_success(void)
 {
-	gpio_init_fake.return_val = 1;
+	gpio_init_fake.return_val = 0;
 
 	TEST_ASSERT_EQUAL(0, led_init());
 
 	TEST_ASSERT_EQUAL(1, gpio_init_fake.call_count);
+}
+
+void test_when_gpio_init_returns_failure_led_init_returns_failure(void)
+{
+	gpio_init_fake.return_val = -1;
+
+	TEST_ASSERT_EQUAL(-1, led_init());
 }
 
 int main(void)
@@ -32,6 +39,7 @@ int main(void)
 	UNITY_BEGIN();
 
 	RUN_TEST(test_when_led_init_is_called_it_calls_gpio_init_and_returns_success);
+	RUN_TEST(test_when_gpio_init_returns_failure_led_init_returns_failure);
 
 	return UNITY_END();
 }
