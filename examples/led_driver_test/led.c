9c2bd366851ecf2cb72c9844541cf91dac5875a4
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-09-09 09:31:08 +0200

    drop: Test if codesonar catches unfreed memory
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

--- examples/led_driver_test/led.c
+++ examples/led_driver_test/led.c
@@ -4,3 +4,27 @@ int led_init(void)
 {
 	return gpio_init();
 }
+
+int fun_with_unfreed_memory(void)
+{
+	char * ptr = (char *)malloc(5);
+}
+
+static int fun_alloc(char ** ptr)
+{
+	*ptr = (char *)malloc(5);
+}
+
+static int fun_free(char ** ptr)
+{
+	free(ptr);
+}
+
+int fun_with_calls_funcs_that_do_malloc_and_free(void)
+{
+	char * ptr;
+
+	fun_alloc(&ptr);
+
+	fun_free(ptr);
+}
