48806a81c846686f00156e78cabc8c5a2f06a141
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-09-13 15:11:33 +0200

    drop: test code
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

--- examples/led_driver_test/led.c
+++ examples/led_driver_test/led.c
@@ -4,3 +4,18 @@ int led_init(void)
 {
 	return gpio_init();
 }
+
+int buggy_led_init(void)
+{
+	int err = gpio_init();
+
+	switch (err) {
+	case 0:
+		return 1;
+	case 1:
+		return 0;
+	case 3:
+		return 0;
+	}
+	return -1;
+}
