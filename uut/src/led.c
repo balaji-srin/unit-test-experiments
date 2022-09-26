26bf97fe10899a89479d4c78ae4f683ee5be380a
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-09-26 22:07:03 +0200

    feat: Demonstrate how to test variadic functions

--- uut/src/led.c
+++ uut/src/led.c
@@ -1,6 +1,18 @@
 #include "gpio.h"
+#include "stddef.h"
+#include "variadic_module.h"
 
 int led_init(void)
 {
 	return gpio_init();
+}
+
+int led_fancy_blink(void)
+{
+	int a;
+	char string[] = "FILL";
+
+	variadic_module_get(2, "%d %s", &a, string, NULL);
+
+	return 0;
 }
